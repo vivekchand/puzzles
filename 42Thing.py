e174522ba86e30281a19c793fa97747d07207822
Vivek Chand <vivekchand19@gmail.com>
2013-03-04 22:01:55 +0530

    SPOJ

--- 42Thing.py
+++ 42Thing.py
@@ -1,7 +1,4 @@
-# Life, the Universe, and Everything
-#Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.
-
-
+# SPOJ Problem 1. Life, the Universe, and Everything
 no = 0
 while True:
 	no = int(input())
